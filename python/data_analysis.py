d9c06285afd55e42ebf2c0d03ba0cfa5ffcf42f8
Praman Patel <patelpraman58@gmail.com>
2026-09-22 23:34:43 +0530

    Complete data analysis and add profit chart

--- python/data_analysis.py
+++ python/data_analysis.py
@@ -23,3 +23,49 @@ category_analysis = df.groupby("Category")[["Sales", "Profit"]].sum()
 
 print("\nSales and Profit by Category:")
 print(category_analysis)
+
+# Sales and Profit by Sub-Category
+subcategory_analysis = df.groupby("Sub-Category")[["Sales", "Profit"]].sum()
+
+print("\nSales and Profit by Sub-Category:")
+print(subcategory_analysis.sort_values("Sales", ascending=False))
+
+# Sales and Profit by Year
+df["Year"] = df["Order Date"].dt.year
+
+yearly_analysis = df.groupby("Year")[["Sales", "Profit"]].sum()
+
+print("\nSales and Profit by Year:")
+print(yearly_analysis)
+
+import matplotlib.pyplot as plt
+
+#sales nd profit by category
+category_analysis.plot(kind="bar")
+
+plt.title("Sales and Profit by Category")
+plt.xlabel("Category")
+plt.ylabel("Amount")
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
+
+#sales by year
+yearly_analysis["Sales"].plot(kind="bar")
+
+plt.title("Sales by Year")
+plt.xlabel("Year")
+plt.ylabel("Sales")
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
+
+#profit by sub-category
+subcategory_analysis["Profit"].short_values().plot(kind="bar")
+
+plt.title("Profit by Sub-Category")
+plt.xlabel("Sub-Category")
+plt.ylabel("profit")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
